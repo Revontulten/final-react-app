e577ee1131791be79721a673f04fcc70f44f422e
Olha Kolodiazhna <92469739+Revontulten@users.noreply.github.com>
2023-09-26 11:58:52 +0000

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,19 @@
 import "./App.css";
 
 export default function App() {
-  return <div className="App">Weather App</div>;
+  return (
+    <div className="App">
+      <h1> Weather App </h1>
+      <footer>
+        This project was coded by Olha Kolodiazhna and is{" "}
+        <a
+          href="httfooters://github.com/Revontulten/final-react-app.git"
+          target="_blank"
+          rel="noreferrer"
+        >
+          open-sourced on GitHub
+        </a>
+      </footer>
+    </div>
+  );
 }
