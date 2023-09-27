76dc93455400f758aefa76aada53fb5eb4b1b338
Olha Kolodiazhna <92469739+Revontulten@users.noreply.github.com>
2023-09-27 12:49:11 +0000

    Update 4 files

--- src/Weather.js
+++ src/Weather.js
@@ -1,53 +1,74 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Weather.css";
+import axios from "axios";
+
 export default function Weather() {
-  return (
-    <div className="Weather">
-      <form>
-        <div className="row">
-          <div className="col-9">
-            <input
-              type="search"
-              placeholder="Type a city here..."
-              className="form-control"
-              autoFocus="on"
-            />
-          </div>
-          <div className="col-3">
-            <input
-              type="submit"
-              value="Search!"
-              className="btn btn-primary w-100"
-            />
-          </div>
-        </div>
-      </form>
+  const [ready, setReady] = useState(false);
+  const [temperature, setTemperature] = useState(null);
 
-      <h1>Carlow</h1>
-      <ul>
-        <li>Wednesday, 07:00</li>
-        <li>Cloudy</li>
-      </ul>
-      <div className="row nt-3">
-        <div className="col-6">
-          <div className="clearfix">
-            <img
-              src="https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png"
-              alt="Partly Cloudy"
-            />
+  function handleResponse(response) {
+    console.log(response.data);
+    setTemperature(response.data.main.temp);
+    setReady(true);
+  }
 
-            <span className="temperature">6</span>
-            <span className="unit">°C</span>
+  if (ready) {
+    return (
+      <div className="Weather">
+        <form>
+          <div className="row">
+            <div className="col-9">
+              <input
+                type="search"
+                placeholder="Type a city here..."
+                className="form-control"
+                autoFocus="on"
+              />
+            </div>
+            <div className="col-3">
+              <input
+                type="submit"
+                value="Search!"
+                className="btn btn-primary w-100"
+              />
+            </div>
           </div>
-        </div>
-        <div className="col-6">
-          <ul>
-            <li>Precipitation: 15%</li>
-            <li>Humidity: 40%</li>
-            <li>Wind:3 km/h</li>
-          </ul>
+        </form>
+
+        <h1>{city}</h1>
+        <ul>
+          <li>Wednesday, 07:00</li>
+          <li>{description}</li>
+        </ul>
+        <div className="row nt-3">
+          <div className="col-6">
+            <div className="clearfix">
+              <img
+                src="https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png"
+                alt="Partly Cloudy"
+              />
+
+              <span className="temperature">{Math.round(temperature)}</span>
+              <span className="unit">°C</span>
+            </div>
+          </div>
+          <div className="col-6">
+            <ul>
+              <li>Precipitation: {precipitation}%</li>
+              <li>Humidity: {humidity}%</li>
+              <li>Wind:{wind} km/h</li>
+            </ul>
+          </div>
         </div>
       </div>
-    </div>
-  );
+    );
+  } else {
+    const apiKey = "094780c710fa4efd669f0df8c3991927";
+    let city = "Carlow";
+    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+
+    axios.get(apiUrl).then(handleResponse);
+
+    return "Loading...";
+  }
 }
