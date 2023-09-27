5857076805c14ed2077fbfff1f656f42bcae9edd
Olha Kolodiazhna <92469739+Revontulten@users.noreply.github.com>
2023-09-27 11:19:07 +0000

    Update 4 files

--- src/Weather.js
+++ src/Weather.js
@@ -10,6 +10,7 @@ export default function Weather() {
               type="search"
               placeholder="Type a city here..."
               className="form-control"
+              autoFocus="on"
             />
           </div>
           <div className="col-3">
@@ -27,13 +28,17 @@ export default function Weather() {
         <li>Wednesday, 07:00</li>
         <li>Cloudy</li>
       </ul>
-      <div className="row">
+      <div className="row nt-3">
         <div className="col-6">
-          <img
-            src="https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png"
-            alt="Partly Cloudy"
-          />
-          6°C
+          <div className="clearfix">
+            <img
+              src="https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png"
+              alt="Partly Cloudy"
+            />
+
+            <span className="temperature">6</span>
+            <span className="unit">°C</span>
+          </div>
         </div>
         <div className="col-6">
           <ul>
